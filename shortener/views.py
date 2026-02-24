e7e7a204e6a16128e58591dfde8e825ea11b3d47
Nsuidara <nsuidara@gmail.com>
2026-02-24 22:13:24 +0100

    change to generics views

--- shortener/views.py
+++ shortener/views.py
@@ -1,23 +1,18 @@
 from typing import Any
 from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect
-from rest_framework import generics, mixins
+from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework.request import Request
-from rest_framework.response import Response
 
 from .models import Short
 from .serializers import ShortCreateSerializer
 from .types import CodeKwargs
 
 
-class ShortCreateView(mixins.CreateModelMixin, generics.GenericAPIView):
+class ShortCreateView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     queryset = Short.objects.all()
     serializer_class = ShortCreateSerializer
-
-    def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        return self.create(request, *args, **kwargs)
 
 
 class ShortRedirectView(generics.GenericAPIView):
